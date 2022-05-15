731c1c9d62912014098bfa982b4213a87e0cbadf
Joren <Joren.dom@outlook.com>
2022-05-15 02:39:43 +0200

    victory/draw function, victoryScreen function

--- java.js
+++ java.js
@@ -1,15 +1,5 @@
 
 
-
-
-
-
-
-
-    
-
-
-
 const drawBoard = () => {
     
     
@@ -21,6 +11,7 @@ const drawBoard = () => {
     let score = false;
 
     const startBtn = document.getElementById("game-start")
+    const resetBtn = document.getElementById("restart");
     const boardToggle = document.getElementById("game-board")
     boardToggle.style.display ="none"
    
@@ -34,19 +25,14 @@ const drawBoard = () => {
            console.log("empty")
         }else{
             boardToggle.style.display = ""
-            console.log("block")
+            console.log("")
         }
         
     }
     startBtn.addEventListener("click", __startScreen)
-    
-   
-
-    
-
-    
-
+    resetBtn.style.display = "none"
     const __squareClicked = (e) => {
+        resetPopup()
         const id = e.target.id;
         console.log(id);
         if(!board[id]){
@@ -54,7 +40,9 @@ const drawBoard = () => {
             e.target.innerText = currentPlayer;
             
           if(winStates(currentPlayer) === true){
-            window.alert(`${currentPlayer} wins!`)
+            console.log(`${currentPlayer} wins!`)
+            const victoryName = document.getElementById("victoryPlayer")
+            victoryName.innerText = `${currentPlayer} wins!`
             score++
           }
            
@@ -94,13 +82,13 @@ const drawBoard = () => {
         square.style = styles;
 
         square.addEventListener("click",__squareClicked)
-    
-        
-        
     });
 
 
-    const resetBtn = document.getElementById("restart");
+    
+
+
+    
     
     const __resetScreen = () =>{
         console.log(board)
@@ -120,80 +108,137 @@ const drawBoard = () => {
     }
 
     resetBtn.addEventListener("click", __resetScreen);
-
-
- // {0,1,2}{0,3,6}{0,4,8}{2,4,6}{2,5,8}{6,7,8}
-    const winStates = (move) => {
-        if(board.includes(null)){
-            if(board[0] === move){
-                if(board[1] === move && board[2] === move){
-                    console.log(`${currentPlayer} wins horizontally`)
-                    return true
-                    
-                }
-            }
-
-            if(board[0] === move){
-                if(board[3] === move && board[6]=== move){
-                    console.log(`${currentPlayer} wins vertically`)
-
-                    return true
-                }
-            }
-
-            if(board[0] === move){
-                if(board[4] === move && board[8]=== move){
-                    console.log(`${currentPlayer} wins diagonally`)
-                    return true
-                }
-            }
-
-            if(board[1] === move){
-                if (board[4] === move && board[7] === move){
-                    console.log(`${currentPlayer} wins vertically`)
-                    return true
-                }
-            }
-
-            if(board[2] === move){
-                if(board[4] === move && board[6] === move){
-                    console.log(`${currentPlayer} wins diagonally`)
-                    return true
-                }
-            }
-
-            if(board[2] === move){
-                if(board[5] === move && board[8]=== move){
-                    console.log(`${currentPlayer} wins vertically`)
-                    return true
-                }
-            }
-
-            if(board[3] === move){
-                if(board[4] === move && board[5]=== move){
-                    console.log(`${currentPlayer} wins vertically`)
-                    return true
-                }
-            }
-
-            if(board[6] === move){
-                if(board[7] === move && board[8]=== move){
-                    console.log(`${currentPlayer} wins horizontally`)
-                    return true
-                }
-            }
-        }else{
-            console.log("draw")
-            window.alert("draw")
-        }
-
-
-        
     
+    const resetPopup = () =>{
+        if(board.includes("O")){
+            resetBtn.style.display = ""
+        }
     }
 
+    let victory = document.getElementById("victory")
+    victory.style.display = "none"
+
+    const closeBtn = document.getElementById("closeBtn")
+    const victoryScreen = () => {
+        
+        if(victory.style.display !== "none"){
+            victory.style.display ="none"
+            
+         }else{
+             victory.style.display = ""
+             console.log("")
+         }
+    } 
+    closeBtn.addEventListener('click' ,victoryScreen)
     
-}
+    
+    
+    
+
+    let winCondition = false;
+
+    const winStates = (move) => {  // {0,1,2}{0,3,6}{0,4,8}{2,4,6}{2,5,8}{6,7,8}
+        
+        
+        if(winCondition === true){
+            __resetScreen()
+            winCondition = false
+        }
+        if(board.includes(null)){
+
+            
+            
+            if(winCondition === false){
+
+            
+                if(board[0] === move){
+                    if(board[1] === move && board[2] === move){
+                        console.log(`${currentPlayer} wins horizontally`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                        
+                    }
+                }
+
+                if(board[0] === move){
+                    if(board[3] === move && board[6]=== move){
+                        console.log(`${currentPlayer} wins vertically`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[0] === move){
+                    if(board[4] === move && board[8]=== move){
+                        console.log(`${currentPlayer} wins diagonally`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[1] === move){
+                    if (board[4] === move && board[7] === move){
+                        console.log(`${currentPlayer} wins vertically`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[2] === move){
+                    if(board[4] === move && board[6] === move){
+                        console.log(`${currentPlayer} wins diagonally`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[2] === move){
+                    if(board[5] === move && board[8]=== move){
+                        console.log(`${currentPlayer} wins vertically`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[3] === move){
+                    if(board[4] === move && board[5]=== move){
+                        console.log(`${currentPlayer} wins vertically`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                    }
+                }
+
+                if(board[6] === move){
+                    if(board[7] === move && board[8]=== move){
+                        console.log(`${currentPlayer} wins horizontally`)
+                        winCondition = true;
+                        victoryScreen()
+                        return true
+                }
+
+                
+            }
+
+
+        }
+
+        }else{
+            console.log("draw")
+            console.log("draw")
+
+        }
+     }
+    }
+
+
+
 
 
 drawBoard();
